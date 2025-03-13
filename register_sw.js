10224fac20edaa13284b1ca5d82f76bdb2fea6b3
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2025-03-12 20:17:35 -0500

    Update register_sw.js

--- register_sw.js
+++ register_sw.js
@@ -1,5 +1,15 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(() => console.log('Service Worker registered successfully.'))
-        .catch(error => console.error('Service Worker registration failed:', error));
+  navigator.serviceWorker.register('sw.js', { scope: '' })
+      .then((registration) => {
+          const data = {
+              type: 'CACHE_URLS',
+              payload: [
+                  location.href,
+                  // cache all files that were pulled in: https://stackoverflow.com/a/55543550/1569320
+                  ...performance.getEntriesByType('resource').map((r) => r.name)
+              ]
+          };
+          registration.installing.postMessage(data);
+      })
+      .catch((err) => console.log('SW registration FAIL:', err));
 }
